d41fe8f71eab6ef03a1c56c93f7ddc4d2a038fcb
Ayman Nedjmeddine <theycallmethedr@gmail.com>
2017-10-13 19:38:01 +0100

    Enable monitoring in production environment only

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+// load environment file
+const dotenv = require('dotenv');
+const { error } = dotenv.config({ silent: true });
+if (error) {
+    console.warn(error.message);
+}
+
 const app = require('./lib/app');
 
-app.listen(app.get('port'), () => {
-    console.log('github-langs running on port', app.get('port'));
-});
+
+app.listen(app.get('port'), () => console.log('github-langs running on port', app.get('port')));
