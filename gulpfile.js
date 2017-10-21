382d9d8cebc9d1fe4ef8f994c88544eb1d630cab
Krzysztof Lubiniecki <klubiniecki@gmail.com>
2017-10-21 10:25:20 +0200

    Refactor for eslint standards

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
-gulp.task('sass', () => {
-    return gulp.src('lib/public/css/base.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('lib/public/css/screen'));
-});
+gulp.task('sass', () => gulp.src('lib/public/css/base.scss').pipe(sass())
+    .pipe(gulp.dest('lib/public/css/screen'))
+);
