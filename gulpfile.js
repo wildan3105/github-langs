b8d4a9e4933cb047179783b27fd3206dbd36cace
Faisal Amir <urmauur@gmail.com>
2020-10-02 14:50:26 +0700

    make scss build compressed and include node module

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
-gulp.task('sass', () => gulp.src('lib/public/css/base.scss').pipe(sass())
+gulp.task('sass', () => gulp.src('lib/public/css/base.scss')
+    .pipe(sass({
+        outputStyle: 'compressed',
+        includePaths: ['node_modules']
+    }).on('error', sass.logError))
     .pipe(gulp.dest('lib/public/css/screen'))
 );
