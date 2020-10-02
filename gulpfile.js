aadb75e23e13b49566fc7eff2a53fb4aa2e17787
Faisal Amir <urmauur@gmail.com>
2020-10-02 15:11:07 +0700

    Rename file minify css using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const rename = require("gulp-rename");
 
 gulp.task('sass', () => gulp.src('lib/public/css/base.scss')
     .pipe(sass({
         outputStyle: 'compressed',
         includePaths: ['node_modules']
     }).on('error', sass.logError))
+    .pipe(
+        rename(function (file) {
+            file.basename += ".min";
+        })
+    )
     .pipe(gulp.dest('lib/public/css/screen'))
 );
