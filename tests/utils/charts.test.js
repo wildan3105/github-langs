0dadbba4c79a3987fc220bfd467153052d06eaef
ashikvarma11 <varmaashik@gmail.com>
2020-10-03 17:24:36 +0530

    Added tests for slugify and sum in chart.js

--- tests/utils/charts.test.js
+++ tests/utils/charts.test.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-undef */
-
-import { chart,sum } from "../../lib/public/js/chart";
-console.log(a)
 const expect = require('expect.js');
+const {sum, slugify} = require('../../lib/public/js/chart');
 
 describe('charts', () => {
-
     it('should return calculated sum of array elements', async () => {
-        const sum = await sum([1,2,3,4]);
-        expect(sum).to.be.equal(10);
+        const total = await sum([1,2,3,4]);
+        expect(total).to.be.equal(10);
+    });
+
+    it('should return slugified version of text', async () => {
+        const slugified = await slugify("This is 123123 some randome test *&Y*& - message");
+        expect(slugified).to.be.eql("this-is-123123-some-randome-test-*&y*&-message");
     });
 });
