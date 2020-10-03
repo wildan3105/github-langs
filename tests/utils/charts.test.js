ce0c7b28695f7926252e12eb3dabe91df6c17b67
ashikvarma11 <varmaashik@gmail.com>
2020-10-03 18:46:36 +0530

    Removed lint issues

--- tests/utils/charts.test.js
+++ tests/utils/charts.test.js
@@ -1,19 +1,19 @@
 /* eslint-disable no-undef */
-const expect = require("expect.js");
-const { sum, slugify } = require("../../lib/public/js/chart");
+const expect = require('expect.js');
+const { sum, slugify } = require('../../lib/public/js/chart');
 
-describe("charts", () => {
-    it("should return calculated sum of array elements", async () => {
-        const total = await sum([1, 2, 3, 4]);
+describe('charts', () => {
+    it('should return calculated sum of array elements',  () => {
+        const total = sum([1, 2, 3, 4]);
         expect(total).to.be.equal(10);
     });
 
-    it("should return slugified version of text", async () => {
-        const slugified = await slugify(
-            "This is 123123 some randome test *&Y*& - message"
+    it('should return slugified version of text',  () => {
+        const slugified = slugify(
+            'This is 123123 some randome test *&Y*& - message'
         );
         expect(slugified).to.be.eql(
-            "this-is-123123-some-randome-test-*&y*&-message"
+            'this-is-123123-some-randome-test-*&y*&-message'
         );
     });
 });
