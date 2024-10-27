bbd1cd612e5a8b1ace4a10f2041f69e9ae11857e
wildan3105 <winistein@gmail.com>
2024-10-27 16:19:04 +0700

    debug: temp disable https from the app

--- src/api/app.js
+++ src/api/app.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-const enforce = require('express-sslify');
+// const enforce = require('express-sslify');
 const app = express();
 
-const { ENV_MISSING_ERROR_MESSAGE } = require('../config');
+// const { ENV_MISSING_ERROR_MESSAGE } = require('../config');
 
-const enableHttps = () => {
-    if (!process.env.ENV) {
-        throw ENV_MISSING_ERROR_MESSAGE;
-    }
-    const enableScript = process.env.ENV === 'local' ? null : app.use(enforce.HTTPS({ trustProtoHeader: true }));
-    return enableScript;
-};
+// const enableHttps = () => {
+//     if (!process.env.ENV) {
+//         throw ENV_MISSING_ERROR_MESSAGE;
+//     }
+//     const enableScript = (process.env.ENV === 'local') ? null : app.use(enforce.HTTPS({ trustProtoHeader: true }));
+//     return enableScript;
+// };
 
-enableHttps();
+// enableHttps();
 
 // template engine setup
 app.set('views', 'src/pages');
