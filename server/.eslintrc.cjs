08ea5dd1f55f25550fd62b8645b24466c694f157
Wildan Syahrun @ Osome <wildan.nahar@osome.com>
2025-04-19 16:53:00 +0700

    fix: format

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -1,29 +1,32 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint', 'import'],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/recommended',
-        'plugin:import/typescript',
-        'prettier', // must come last
-    ],
-    rules: {
-        'import/order': [
-        'warn',
-        {
-            groups: [['builtin', 'external'], ['internal', 'parent', 'sibling', 'index']],
-            'newlines-between': 'always',
-        },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier', // must come last
+  ],
+  rules: {
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent', 'sibling', 'index'],
         ],
-        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    },
-    env: {
-        node: true,
-        es2021: true,
-    },
+        'newlines-between': 'always',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+  env: {
+    node: true,
+    es2021: true,
+  },
 };
